e9309726fd750dd4981608c35726adeb0f367eaf
metacoding <getinthere@naver.com>
2023-07-04 21:31:00 +0900

    시큐리티 세션 확인 테스트 완료

--- src/main/java/shop/mtcoding/tddbank/TddbankApplication.java
+++ src/main/java/shop/mtcoding/tddbank/TddbankApplication.java
@@ -2,9 +2,19 @@ package shop.mtcoding.tddbank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class TddbankApplication {
+
+    @GetMapping("/tdd")
+    public String tdd(@AuthenticationPrincipal UserDetails userDetails){
+        return userDetails.getUsername();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(TddbankApplication.class, args);
